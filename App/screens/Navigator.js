0c0e4a0c1969d13f8d75ffecce4ba245804f68b2
YasuriRanasinghe <ar97984@fhss.sjp.ac.lk>
2022-08-29 19:47:29 +0530

    change stack navigator

--- App/screens/Navigator.js
+++ App/screens/Navigator.js
@@ -6,15 +6,15 @@ import App from '../view/NewMovie';
 
 const Stack = createNativeStackNavigator();
 
-function app () {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='NewMovie'>
-                <Stack.Screen name ='ScreenOne' component={ScreenOne}/>
-                <Stack.Screen name ='NewMovie' component={App}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+function app() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NewMovie">
+        <Stack.Screen name="ScreenOne" component={ScreenOne} />
+        <Stack.Screen name="NewMovie" component={App} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default app;
